0eaf866d1b7ded227f4885e41247bdcf72e82c1c
KrisJe <kristof.kiekens@gmail.com>
2024-05-06 10:51:13 +0200

    Update reading and writing database stage 1 step 2

--- src/models.rs
+++ src/models.rs
@@ -2,16 +2,19 @@
 
 use std::{collections::HashMap};
 
+use serde::{Serialize, Deserialize};
+
+
+#[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
 pub enum Status {
-    // TODO: add fields (make sure the fields are public)
-   InProgress,
-   Closed,
-   Open,
-   Resolved,
+    Open,
+    InProgress,
+    Resolved,
+    Closed
 }
 
+#[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
 pub struct Epic {
-    // TODO: add fields (make sure the fields are public)
     pub name: String,
     pub description: String,
     pub status: Status,
@@ -20,41 +23,93 @@ pub struct Epic {
 
 impl Epic {
     pub fn new(name: String, description: String) -> Self {
-        //todo!() // by default the status should be set to open and the stories should be an empty vector
-        
-        Self { 
-              name,
-              description,
-              status: Status::Open, 
-              stories : vec![] 
-            }
+        Self {
+            name,
+            description,
+            status: Status::Open,
+            stories: vec![]
+        }
     }
 }
 
+#[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
 pub struct Story {
-    // TODO: add fields (make sure the fields are public)
-    pub name : String,
-    pub description : String,
+    pub name: String,
+    pub description: String,
     pub status: Status,
 }
 
 impl Story {
     pub fn new(name: String, description: String) -> Self {
-        //todo!() // by default the status should be set to open
-        Self { name,
-             description,
-             status: Status::Open
-            }
+        Self {
+            name,
+            description,
+            status: Status::Open,
+        }
     }
 }
 
+#[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
 pub struct DBState {
-    // This struct represents the entire db state which includes the last_item_id, epics, and stories
-    // TODO: add fields (make sure the fields are public)
-
     pub last_item_id: u32,
     pub epics: HashMap<u32, Epic>,
     pub stories: HashMap<u32, Story>
+}
 
 
+
+impl DBState {
+    pub fn new() -> Self {
+        Self {
+            last_item_id  : 0,
+            epics: HashMap::new(),    
+            stories: HashMap::new(),       
+        }
+    }
+}
+
+mod tests {
+    mod database {
+        use std::collections::HashMap;
+        
+        use crate::models::{DBState, Epic, Status, Story};
+        #[test]
+        fn create_new_DBState() {
+           let story = Story {
+            name: "epic 1".to_owned(),
+            description: "epic 1".to_owned(),
+            status: Status::Open,
+        };
+
+        let apic2 = Epic::new("epic 1".to_owned(),"epic 1".to_owned());
+
+        let epic = Epic {
+            name: "epic 1".to_owned(),
+            description: "epic 1".to_owned(),
+            status: Status::Open,
+            stories: vec![2],
+        };
+         //assert_eq!(epic.status, Status::Open); doesn't work
+        //assert_eq!(story.status, Status::Open);
+        assert!(matches!(epic.status,Status::Open));
+        assert!(matches!(story.status,Status::Open));
+
+        let mut stories = HashMap::new();
+        stories.insert(2, story);
+
+        let mut epics = HashMap::new();
+        epics.insert(1, epic);
+
+        let state = DBState {
+            last_item_id: 2,
+            epics,
+            stories,
+        };       
+
+        assert!(matches!(state.last_item_id,2));
+
+       
+        }
+
+    }
 }
